f7dface1d706500b2fc03adf6d4263d7c30e17aa
Jess Bees <hi@toomanybees.com>
2019-11-25 03:22:02 -0500

    Ask for confirmation first

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,16 @@ use futures::{executor, future};
 
 fn main() {
     let message = heaven::random_post();
-    println!("{}", message);
+    println!("Should we post this? (y/n)\n\n{}\n", message);
+
+    let mut input = String::new();
+    if let Ok(_) = std::io::stdin().read_line(&mut input) {
+        if !input.to_lowercase().starts_with('y') {
+            return;
+        }
+    } else {
+        return;
+    }
 
     if let Err(_) = dotenv::dotenv() {
         eprintln!("No variables loaded from .env");
