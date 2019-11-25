64196d56eb47353852c5c67320be2fab090f3bdb
Jess Bees <hi@toomanybees.com>
2019-11-25 01:13:39 -0500

    More generation variations

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,39 @@
 extern crate rand;
 use rand::seq::SliceRandom;
+use chrono::{Datelike, Weekday};
 
-const DAY: &str = "Friday";
+fn today() -> &'static str {
+    match chrono::Local::today().weekday() {
+        Weekday::Sun => "Sunday",
+        Weekday::Mon => "Monday",
+        Weekday::Tue => "Tuesday",
+        Weekday::Wed => "Wednesday",
+        Weekday::Thu => "Thursday",
+        Weekday::Fri => "Friday",
+        Weekday::Sat => "Saturday",
+    }
+}
 
-fn random_declaration() -> &'static str {
-    const STATEMENTS: &[&'static str] = &[
-        "good day",
-        "it's a good day",
-        "feels like a good day",
-        "great day",
-        "sure is a fine day",
-        "I think it's a day",
+fn random_declaration(join: bool) -> String {
+    const STATEMENTS: &[(Option<&'static str>, &'static str)] = &[
+        (Some("it's a"), "good day"),
+        (Some("it"), "feels like a good day"),
+        (Some("it's a"), "great day"),
+        (Some("it"), "sure is a fine day"),
+        (None, "I think it's a day"),
     ];
     let mut rng = rand::thread_rng();
-    *STATEMENTS.choose(&mut rng).unwrap()
+    STATEMENTS.choose(&mut rng).map(|&pair| {
+        if join {
+            if let Some(s) = pair.0 {
+                format!("{} {}", s, pair.1)
+            } else {
+                pair.1.into()
+            }
+        } else {
+            pair.1.into()
+        }
+    }).unwrap()
 }
 
 fn random_question() -> &'static str {
@@ -29,15 +49,16 @@ fn random_question() -> &'static str {
 
 fn random_action() -> String {
     const ACTIONS: &[&'static str] = &[
-        "to be singing %S in your head",
-        "to sing %S in the back of your head",
+//        "to be singing %S in your head",
         "to get %S stuck in your head",
+        "to have %S stuck in your head",
         "for getting %S stuck in your head",
         "for %S to be stuck in your head",
     ];
     const SONGS: &[(&'static str, u8)] = &[
         ("'Heaven Is A Place On Earth'", 3),
         ("Belinda Carlisle's 'Heaven Is A Place On Earth'", 1),
+//        ("'Heaven Is A Place On Earth'", 1),
     ];
     let mut rng = rand::thread_rng();
     let action = ACTIONS.choose(&mut rng).unwrap();
@@ -51,10 +72,11 @@ fn random_action() -> String {
 fn random_incidental() -> String {
     const INCIDENTALS: &[&'static str] = &[
         "Well it's %D, so",
+        "Well it's a %D, so",
         "Is it %D? Then",
     ];
     let mut rng = rand::thread_rng();
-    INCIDENTALS.choose(&mut rng).unwrap().replace("%D", DAY)
+    INCIDENTALS.choose(&mut rng).unwrap().replace("%D", &today())
 }
 
 fn random_answer(join: bool) -> String {
@@ -65,6 +87,7 @@ fn random_answer(join: bool) -> String {
         (None, "heck yeah."),
         (Some("it"), "feels like it."),
         (Some("it"), "sure feels like it."),
+        (None, "today is THE perfect day."),
     ];
     let mut rng = rand::thread_rng();
     ANSWERS
@@ -95,16 +118,24 @@ const VARIANTS: &[fn() -> String] = &[
     || {
         format!(
             "It's {day}, so {declaration} {action}.",
-            day = DAY,
-            declaration = capitalize(random_declaration()),
+            day = today(),
+            declaration = random_declaration(true),
             action = random_action()
         )
     },
     || {
         format!(
             "{day}? {declaration} {action}.",
-            day = DAY,
-            declaration = capitalize(random_declaration()),
+            day = today(),
+            declaration = capitalize(&random_declaration(false)),
+            action = random_action()
+        )
+    },
+    || {
+        format!(
+            "{day} again? {declaration} {action}.",
+            day = today(),
+            declaration = capitalize(&random_declaration(false)),
             action = random_action()
         )
     },
